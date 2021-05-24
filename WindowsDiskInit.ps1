cc1e07c6e75618d909554d7c3c3a4df76eaf3e6e
power-sandy8051 <62498823+power-sandy8051@users.noreply.github.com>
2021-05-24 17:10:10 +0530

    Update WindowsDiskInit.ps1

--- WindowsDiskInit.ps1
+++ WindowsDiskInit.ps1
@@ -2,7 +2,7 @@ $disk = Get-Disk | Where partitionstyle -eq 'raw' | sort number
 $existing_disk_count = Get-Disk | Where partitionstyle -ne 'raw' | sort number | measure
 $existing_disk_count = $existing_disk_count.Count
 
-$letters = 70..89 | ForEach-Object { [char]$_ }
+$letters = 69..89 | ForEach-Object { [char]$_ }
 $label = "Local Disk"
 
 $driveLetter = $letters[$existing_disk_count].ToString()
