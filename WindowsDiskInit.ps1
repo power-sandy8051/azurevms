a5b7bb67d21e43bd429d85d8e5d3491cfebf5744
power-sandy8051 <62498823+power-sandy8051@users.noreply.github.com>
2021-05-25 09:04:05 +0000

    Update WindowsDiskInit.ps1

--- WindowsDiskInit.ps1
+++ WindowsDiskInit.ps1
@@ -6,6 +6,10 @@ param(
 
 $disk = Get-Disk | Where partitionstyle -eq 'raw' | sort number
 
+$driveLetter = $driveLetter.Trim("'")
+$driveLetter = $driveLetter.Trim('"')
+$result = [char]$driveLetter[0]
+
 $label = "Local Disk"
 
 if ($diskSize -ge 2048){
@@ -17,5 +21,5 @@ else{
 
 $disk | 
 Initialize-Disk -PartitionStyle $partitionStyle -PassThru |
-New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+New-Partition -UseMaximumSize -DriveLetter $result |
 Format-Volume -FileSystem NTFS -NewFileSystemLabel $label -Confirm:$false -Force
