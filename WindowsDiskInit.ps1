9b8559e70c3b0337f16bbcba9d5b0650009ace43
power-sandy8051 <62498823+power-sandy8051@users.noreply.github.com>
2021-05-25 08:05:31 +0000

    Update WindowsDiskInit.ps1

--- WindowsDiskInit.ps1
+++ WindowsDiskInit.ps1
@@ -1,20 +1,19 @@
 [CmdletBinding()]
 param(
-  [Parameter(Mandatory = $true)][object]$scriptParams
+    [string]$diskSize,
+    [string]$driveLetter
 )
 
 $disk = Get-Disk | Where partitionstyle -eq 'raw' | sort number
 
 $label = "Local Disk"
 
-if ([int]$scriptParams.diskSize -ge 2048){
+if ([int]diskSize -ge 2048){
   $partitionStyle = "GPT"
 }
 else{
   $partitionStyle = "MBR"
 }
-
-$driveLetter = $scriptParams.driveLetter.ToString()
 
 $disk | 
 Initialize-Disk -PartitionStyle $partitionStyle -PassThru |
