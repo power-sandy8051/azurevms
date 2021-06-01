7c9e08f9e083f8a8be8c3c58f407bc55bb3e97e4
power-sandy8051 <62498823+power-sandy8051@users.noreply.github.com>
2021-06-01 09:09:57 +0000

    Update LinuxDiskPart.sh

--- LinuxDiskPart.sh
+++ LinuxDiskPart.sh
@@ -3,7 +3,7 @@
 directory=$1
 
 drive_letter=(lsblk -r --output NAME,MOUNTPOINT | awk -F \/ '/sd/ { dsk=substr($1,1,3);dsks[dsk]+=1 } END { for ( i in dsks ) { if (dsks[i]==1) print i } }')
-IFS=' ' read -r -a array <<< "$disk"
+IFS=' ' read -r -a array <<< "$drive_letter"
 myDisk=${array[0]}
 # Partition the drive /dev/sdc.
 # Read from standard input provide the options we want.
@@ -29,13 +29,13 @@ EOF
 # Write a file system to the partition.
 #  ext4 creates an ext4 filesystem.
 #  /dev/sdc1 is the device name.
-sudo mkfs -t ext4 /dev/${myDisk}1
+sudo mkfs -t ext4 /dev/$myDisk1
 
 # Create the /uploads directory, which we'll use as our mount point.
-sudo mkdir /${directory}
+sudo mkdir /$directory
 
 # Attach the disk to the mount point.
-sudo mount /dev/${myDisk}1 /${directory}
+sudo mount /dev/$myDisk1 /$directory
 
 # Get the UUID of the new drive, /dev/sdc1, and save it as a variable.
 UUID=$(sudo -i blkid | grep '/dev/${myDisk}1 ' | perl -pe 's/.+([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}).+/$1/')
